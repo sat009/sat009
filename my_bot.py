d34ce4646fe22d844ff93dd227795f23c75964d8
sat009 <51149455+sat009@users.noreply.github.com>
2019-06-05 20:40:10 +0300

    'amin'

--- my_bot.py
+++ my_bot.py
@@ -1,11 +1,11 @@
 import requests  
 import datetime
-
+token = "769395327:AAEx_PTGIdNSzcJ8kMAK4NZVH0g9k3TrrXM"
 class BotHandler:
 
     def __init__(self, token):
         self.token = token
-        self.api_url = "https://api.telegram.org/bot{769395327:AAEx_PTGIdNSzcJ8kMAK4NZVH0g9k3TrrXM}/".format(token)
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
     def get_updates(self, offset=None, timeout=30):
         method = 'getUpdates'
@@ -29,3 +29,43 @@ class BotHandler:
             last_update = get_result[len(get_result)]
 
         return last_update
+
+        greet_bot = BotHandler(token)  
+greetings = ('здравствуй', 'привет', 'ку', 'здорово')  
+now = datetime.datetime.now()
+
+
+def main():  
+    new_offset = None
+    today = now.day
+    hour = now.hour
+
+    while True:
+        greet_bot.get_updates(new_offset)
+
+        last_update = greet_bot.get_last_update()
+
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
+
+        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+            today += 1
+
+        new_offset = last_update_id + 1
+
+if __name__ == '__main__':  
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
